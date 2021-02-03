d286a1daf6119e68ae91771ca12b7f016aec7b85
Aditya Singh <68491442+adityasingh2509@users.noreply.github.com>
2021-02-03 17:52:12 +0530

    Tweet and NewQuote Functionality Added

--- random-quote-generator/js/scripts.js
+++ random-quote-generator/js/scripts.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('primary-quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 //Get Quote From API
 async function getQuote() {
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; //Proxy URL to avoid CORS error
@@ -5,13 +11,38 @@ async function getQuote() {
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+
+    //Credit Author as Anonymous if name isn't received in API.
+    if (data.quoteAuthor === ''){
+      authorText.innerText = 'Anonymous';
+    } else{
+      authorText.innerText = data.quoteAuthor; 
+    }
+
+    //Reduce Font Size for long quotes > 120 chars in length
+    if (data.quoteText.length > 120){
+      quoteText.classList.add('long-quote');
+    }else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;//1st quoteText is Userdefined, the 2nd is from API
   } catch (error) {
     getQuote(); //Added to refetch in case of API Error.
-    console.log('whoops, no quote',error);
+
   }
 }
 
+//Tweet Code
+function tweetQuote(){
+  const quote = quoteText.innerText;  //Userdefined quoteText
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 //On Load
 getQuote();
