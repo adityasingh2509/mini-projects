6e4aa66a9e16b5ee7a2aa31d0ef99849e5fe16d5
Aditya Singh <68491442+adityasingh2509@users.noreply.github.com>
2021-02-04 18:22:21 +0530

    Faster Initial Load

--- infinite-scroller/js/scripts.js
+++ infinite-scroller/js/scripts.js
@@ -7,9 +7,9 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
+let count = 5; // For Faster Initial Load
 const apiKey = 'ryyqQvDkbzlk85fHOP7HYsf8z-oVk3QjgwnDpzMitRE';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`; // For Faster Initial Load
 
 
 // Check if all images were loaded
@@ -18,6 +18,9 @@ function imageLoaded() {
     if (imagesLoaded === totalImages) {
         ready = true;
         loader.hidden = true;
+        count = 30;
+        apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
     }
 }
 
