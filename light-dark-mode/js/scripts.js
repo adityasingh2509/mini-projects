5e9f228b9fbb33c941eafcff833f516f9f485d6d
Aditya Singh <68491442+adityasingh2509@users.noreply.github.com>
2021-02-10 00:10:31 +0530

    switchTheme Function Added

--- light-dark-mode/js/scripts.js
+++ light-dark-mode/js/scripts.js
@@ -1,12 +1,24 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+
+
 
 
 // Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute("data-theme", 'dark');
+        darkMode();
+
     } else {
-        document.documentElement.removeAttribute("data-theme", 'dark')
+        document.documentElement.removeAttribute("data-theme", 'dark');
+        lightMode();
     }
 }
 
