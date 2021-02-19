cc73d7d1fecd5ae6b5d9c22b4380ad0d75d6eb3a
Aditya Singh <68491442+adityasingh2509@users.noreply.github.com>
2021-02-19 16:31:40 +0530

    Updated App.js

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,36 +1,32 @@
 import './App.css';
 import { Component } from 'react';
+import { CardList } from "./components/card-list/card-list.component.jsx";
 
-class App extends Component{
-    constructor() {
-        super();
+class App extends Component {
+  constructor() {
+    super();
 
-        this.state = {
-            monsters: []
-        };
-    }
+    this.state = {
+      monsters: []
+    };
+  }
 
-    // Life Cycle Method
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json()) // Converting response to JSON.
-        .then(users => this.setState({monsters: users}))
-    }
+  // Life Cycle Method
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json()) // Converting response to JSON.
+      .then(users => this.setState({ monsters: users }))
+  }
 
 
-    render(){
-        return(
-            <div className="App">
-                {
-                    this.state.monsters.map(monster => (
-                        <h1 key = {monster.id}> {monster.name} </h1>
-                    ))
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters = {this.state.monsters} />
+      </div>
+    )
+  }
 }
 
 export default App;
 
-  
